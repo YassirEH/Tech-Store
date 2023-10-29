ae11a30f516779a5b0fad23a431ee4079cae956f
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-10-29 14:56:20 +0100

    added search by First Name, Last Name, as well as added bootstrap

--- aspcrud-frontend/src/components/Buyer.js
+++ aspcrud-frontend/src/components/Buyer.js
@@ -1,5 +1,6 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function BuyersList() {
   const [Id, setId] = useState("");
@@ -7,6 +8,9 @@ function BuyersList() {
   const [LName, setLName] = useState("");
   const [Email, setEmail] = useState("");
   const [Buyers, setBuyers] = useState(null);
+  const [filterName, setFilterName] = useState("");
+  const [filterLastName, setFilterLastName] = useState("");
+  const [filteredBuyers, setFilteredBuyers] = useState(null);
 
   useEffect(() => {
     (async () => await Load())();
@@ -18,7 +22,7 @@ function BuyersList() {
 
       if (Array.isArray(result.data.data)) {
         setBuyers(result.data.data);
-        console.log(result.data.data);
+        setFilteredBuyers(result.data.data);
       } else {
         console.error("API response does not contain an array:", result.data);
       }
@@ -46,10 +50,9 @@ function BuyersList() {
   }
 
   async function editBuyer(Buyer) {
-    setFName(Buyer.fName)
+    setFName(Buyer.fName);
     setLName(Buyer.lName);
     setEmail(Buyer.email);
-
     setId(Buyer.id);
   }
 
@@ -77,7 +80,7 @@ function BuyersList() {
       console.error('Id is not set. Cannot update the buyer.');
       return;
     }
-  
+
     try {
       await axios.put(`https://localhost:7156/api/Buyer/Update Buyer?buyerId=${Id}`, {
         fName: FName,
@@ -95,91 +98,150 @@ function BuyersList() {
     }
   }
 
+  function filterBuyersByName() {
+    setFilteredBuyers(Buyers);
+    const filterValue = filterName.toLowerCase();
+    const filteredBuyers = Buyers.filter((buyer) => {
+      const buyerFirstName = buyer.fName.toLowerCase();
+      return buyerFirstName.includes(filterValue);
+    });
+    setFilteredBuyers(filteredBuyers);
+  }
+
+  function filterBuyersByLastName() {
+    setFilteredBuyers(Buyers);
+    const filterValue = filterLastName.toLowerCase();
+    const filteredBuyers = Buyers.filter((buyer) => {
+      const buyerLastName = buyer.lName.toLowerCase();
+      return buyerLastName.includes(filterValue);
+    });
+    setFilteredBuyers(filteredBuyers);
+  }
 
   return (
-    <div>
-      <h1>Buyers List</h1>
-      <div className="container mt-4">
-        <form>
-          <div className="form-group">
-            <label>First Name</label>
+    <div className="container mt-4">
+      <h1 className="text-center">Buyers List</h1>
+
+      <form className="mb-4">
+        <div className="form-row">
+          <div className="col">
             <input
               type="text"
               className="form-control"
-              id="FName"
+              placeholder="First Name"
               value={FName}
               onChange={(event) => {
                 setFName(event.target.value);
               }}
-            /><br></br>
-            <label>Last Name</label>
+            />
+          </div>
+          <div className="col">
             <input
               type="text"
               className="form-control"
-              id="LName"
+              placeholder="Last Name"
               value={LName}
               onChange={(event) => {
                 setLName(event.target.value);
               }}
             />
           </div>
-          <div className="form-group">
-            <label>Email</label>
+          <div className="col">
             <input
               type="text"
               className="form-control"
-              id="Email"
+              placeholder="Email"
               value={Email}
               onChange={(event) => {
                 setEmail(event.target.value);
               }}
             />
           </div>
-          <div>
-          <button className="btn btn-primary mt-4" onClick={save}>
-            Register
-          </button>
-          <button className="btn btn-warning mt-4" onClick={UpdateBuyer}>
-            Update
-          </button>
+          <div className="col">
+            <button className="btn btn-primary" onClick={save}>
+              Register
+            </button>
+            <button className="btn btn-warning" onClick={UpdateBuyer}>
+              Update
+            </button>
+          </div>
         </div>
-        </form>
-      </div>
-      <br></br>
+      </form>
 
-      <table className="table table-dark" align="center">
-        <thead>
+      <div className="mb-4">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter First Name"
+            value={filterName}
+            onChange={(event) => {
+              setFilterName(event.target.value);
+            }}
+          />
+          <div className="input-group-append">
+            <button className="btn btn-primary" onClick={filterBuyersByName}>
+              Search by First Name
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter Last Name"
+            value={filterLastName}
+            onChange={(event) => {
+              setFilterLastName(event.target.value);
+            }}
+          />
+          <div className="input-group-append">
+            <button className="btn btn-primary" onClick={filterBuyersByLastName}>
+              Search by Last Name
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <table className="table table-striped">
+        <thead className="thead-dark">
           <tr>
-          <th scope="col">Buyer Id</th>
+            <th scope="col">Buyer Id</th>
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
             <th scope="col">Email</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
-        {Buyers === null ? (
-          <tbody>
+        <tbody>
+          {filteredBuyers === null ? (
             <tr>
-              <td>Loading...</td>
+              <td colSpan="5" className="text-center">Loading...</td>
             </tr>
-          </tbody>
-        ) : (
-          Buyers.map(function fn(Buyer, index) {
-            return (
-              <tbody key={index}>
-                <tr>
+          ) : (
+            filteredBuyers.map(function fn(Buyer, index) {
+              return (
+                <tr key={index}>
                   <td>{Buyer.id}</td>
                   <td>{Buyer.fName}</td>
                   <td>{Buyer.lName}</td>
                   <td>{Buyer.email}</td>
                   <td>
-                    <button type="button" className="btn btn-warning" onClick={() => {
+                    <button
+                      type="button"
+                      className="btn btn-warning btn-sm"
+                      onClick={() => {
                         editBuyer(Buyer);
-                      }}>
+                      }}
+                    >
                       Edit
                     </button>
                     <button
                       type="button"
-                      className="btn btn-danger"
+                      className="btn btn-danger btn-sm"
                       onClick={() => {
                         DeleteBuyer(Buyer.id);
                       }}
@@ -188,13 +250,14 @@ function BuyersList() {
                     </button>
                   </td>
                 </tr>
-              </tbody>
-            );
-          })
-        )}
+              );
+            })
+          )}
+        </tbody>
       </table>
     </div>
   );
 }
 
 export default BuyersList;
+
